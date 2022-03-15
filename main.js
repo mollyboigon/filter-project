1ac2d375aa7180c7121ff7d1595a13f4e0dde1be
Molly Boigon <molly.boigon79@journalism.cuny.edu>
2022-03-15 19:03:46 -0400

    added h3

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const select = document.querySelector(".filter")
 select.addEventListener("change", function (e) {
     const chosen_city = e.target.value;
     const currently_visible = document.querySelectorAll(".item.active")
-    const make_visible = document.querySelectorAll(`.item.${chosen_city}`)
+    const make_visible = document.querySelectorAll(`.item.${chosen_city}, .item.national`)
 
     currently_visible.forEach(function (cities) {
         cities.classList.remove("active");
